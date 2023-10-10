4017af145a4ec7538395c738c6973a32181e5596
Miguelc94 <m@miguelc94.com.co>
2023-10-10 16:24:27 -0500

    update

--- DB.sql
+++ DB.sql
@@ -1,30 +1,9 @@
-drop schema pn2;
-create schema pn2;
+drop schema if exists pn2;
+create schema if not exists pn2;
 
 use pn2;
 
 # use miguelcc_pn2;
-
-create trigger PRESTAMO
-after update on `productos`
-for each row
-insert into `prestamos` values (new.fecha_devolucion, new.fecha_prestamo, new.solicitante, new.nombre_producto);
-
-
-insert into usuarios values
-('admin', 'admin');
-
-insert into categorias (nombre) values
-('Juegos de mesa'),
-('Consolas'),
-('Cartas'),
-('Balones de Futbol'),
-('Balones de Voleibol');
-
-insert into productos (categoria, estado, nombre) values
-(1, 1, 'Parques'),
-(2, 1, 'PS4'),
-(4, 0, 'Balon Futsal');
 
 create table if not exists usuarios(
 usuario varchar(100) primary key,
@@ -43,3 +22,23 @@ estado boolean,
 codigoCategoria int(10),
 key `foreing categoria` (codigoCategoria)
 );
+
+insert into usuarios values
+('admin', 'admin');
+
+insert into categorias (nombre) values
+('Juegos de mesa'),
+('Consolas'),
+('Cartas'),
+('Balones de Futbol'),
+('Balones de Voleibol');
+
+insert into productos (codigoCategoria, estado, nombre) values
+(1, 1, 'Parques'),
+(2, 1, 'PS4'),
+(4, 0, 'Balon Futsal');
+
+create trigger PRESTAMO
+after update on `productos`
+for each row
+insert into `prestamos` values (NEW.fecha_devolucion, new.fecha_prestamo, new.solicitante, new.nombre_producto);
